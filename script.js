dff4fc8ac37eb8d56e3c9405f744dace088f6d06
Jeferson Fernandes <dev.jefersonl@gmail.com>
2024-03-05 15:39:20 -0300

    criação do loop para mover cada número do cpf para uma posição dentro de um array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const userCpfInput = '030.785.210-52';
 let usrCpfConvertido = userCpfInput.replace(/\./g, '').replace(/\-/g, '');
 let userCpfArray = [];
+
+for(let i = 0; i < usrCpfConvertido.length; i++) {
+    let numeroLoop  = usrCpfConvertido.slice(i, (i+1))
+    userCpfArray.push(numeroLoop);
+}
