9a43fb90341a7e98befb3c8a8b4397b0ce1d4ac6
Jeferson Fernandes <dev.jefersonl@gmail.com>
2024-03-05 15:55:30 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const userCpfInput = '030.785.210-52';
-let usrCpfConvertido = userCpfInput.replace(/\./g, '').replace(/\-/g, '');
+let userCpfFormatado = userCpfInput.replace(/\./g, '').replace(/\-/g, '');
+let userCpfConvertido = parseInt(userCpfFormatado);
 let userCpfArray = [];
 
-for(let i = 0; i < usrCpfConvertido.length; i++) {
-    let numeroLoop  = usrCpfConvertido.slice(i, (i+1))
+for(let i = 0; i < userCpfFormatado.length; i++) {
+    let numeroLoop  = userCpfConvertido.slice(i, (i+1))
     userCpfArray.push(numeroLoop);
 }
+
+console.log(typeof(userCpfArray));
+
+function verificarCpf() {
+    let resultadoLoop = [];
+    let contador = 0
+    for(let i = 10; i > 1; i--) {
+        console.log(i);
+        resultadoLoop.push(teste);
+    }
+
+    console.log(resultadoLoop);
+};
