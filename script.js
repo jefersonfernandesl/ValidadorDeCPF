a70299e8322b24f94fa8947ba4cd55ec364fc9f9
Jeferson Fernandes <dev.jefersonl@gmail.com>
2024-03-07 15:40:35 -0300

    conversao para poo

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
-const userCpfInput = '030.785.210-52';
-let userCpfFormatado = userCpfInput.replace(/\./g, '').replace(/\-/g, '');
-let userCpfConvertido = parseInt(userCpfFormatado);
-let userCpfArray = [];
+function UserCpfVerify(userCpf) {
 
-for(let i = 0; i < userCpfFormatado.length; i++) {
-    let numeroLoop  = userCpfConvertido.slice(i, (i+1))
-    userCpfArray.push(numeroLoop);
-}
+    this.userCpf = userCpf.replace(/-/g, "").replace(/\./g, "");
+    this.userCpfToArray = this.convertToArray(this.userCpf);
 
-console.log(typeof(userCpfArray));
+};
 
-function verificarCpf() {
-    let resultadoLoop = [];
-    let contador = 0
-    for(let i = 10; i > 1; i--) {
-        console.log(i);
-        resultadoLoop.push(teste);
+UserCpfVerify.prototype.returnResult = function () {
+
+    let userCpfArrayResult = this.userCpfToArray;
+
+    let count = 0;
+    let arrayResults = [];
+
+    for (let i = userCpfArrayResult; i >= 2; i--) {
+        let number = parseInt(userCpfArrayResult[count]);
+        arrayResults.push(number * i);
+        count++;
     }
 
-    console.log(resultadoLoop);
+    let someArray = this.someArrayFunction(parseInt(arrayResults));
+
+    let calResult = 11 - (someArray % 11);
+    if (calResult > 9) {
+        calResult = 0;
+    };
+
+    let teste2 = teste22(calResult);
+
+    function teste22(value) {
+        let count = 0;
+        let arrayResults = [value];
+        userCpfArrayResult.push();
+        for (let i = 11; i >= 2; i--) {
+
+            let number = parseInt(userCpfArrayResult[count]);
+            arrayResults.push(number * i);
+            count++;
+
+        };
+
+        let someArray = this.someArrayFunction(parseInt(arrayResults));
+        let calResult = 11 - (someArray % 11);
+        if (calResult > 9) {
+
+            calResult = 0;
+
+        };
+
+        return calResult;
+    }
+
 };
+
+UserCpfVerify.prototype.convertToArray = function (wordToArray) {
+    let arrayWord = [];
+    for (let j = 0; j < 11; j++) {
+
+        arrayWord.push(wordToArray.slice(j, (j + 1)));
+
+    }
+    return arrayWord;
+};
+
+UserCpfVerify.prototype.someArrayFunction = function (arrayS) {
+    let resultSoma = 0;
+    for (let i = 0; i < arrayS.length; i++) {
+
+        resultSoma = resultSoma + arrayS[i];
+
+    }
+
+    return resultSoma;
+};
+
+const userOne = new UserCpfVerify('479.607.870-34');
+userOne.returnResult();
+
+
+
+
+
